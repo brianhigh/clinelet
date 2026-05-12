1ffeed82d52c00fcf81f8d3e1c6ec7cd5f8f5a18
Brian High <bhigh@live.com>
2026-05-12 10:42:04 -0700

    added file tests

--- scripts/md_to_html.sh
+++ scripts/md_to_html.sh
@@ -1,10 +1,72 @@
 #!/usr/bin/env bash
 
+
+
+
+
 # Convert a markdown file to html and save in export_dir
+
+
 # Set HTML title to first header in markdown file
 
+
+
+
+
+# Detect pandoc
+
+
+if command -v pandoc >/dev/null 2>&1; then
+
+
+    PD=(pandoc)
+
+
+else
+
+
+    echo "pandoc not found" >&2
+
+
+    exit 1
+
+
+fi
+
+
+
+
+
+HTH='scripts/header_to_title.lua'
+
+
+if [[ ! -f "$HTH" ]]; then 
+
+
+    echo "$HTH not found" >&2
+
+
+    exit 2
+
+
+fi
+
+
+
+
+
 export_dir='export'
+
+
 mkdir -p "$export_dir"
+
+
 base=$(basename "${1%.*}")
-pandoc -s --lua-filter=scripts/header_to_file.lua --to html "$1" -o "${export_dir}/${base}.html"
+
+
+"$PD" -s --lua-filter="$HTH" --from markdown --to html "$1" -o "${export_dir}/${base}.html"
+
+
+
+
 
